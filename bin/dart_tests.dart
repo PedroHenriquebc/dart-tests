dc81a3a3bdfdde2c9c46e97ad496439099622eb6
Pedro Henrique <pedrohbc96@gmail.com>
2023-09-19 20:11:25 -0300

    feat: adiciona arquivos iniciais

---
+++ bin/dart_tests.dart
@@ -0,0 +1,5 @@
+import 'package:dart_tests/dart_tests.dart' as dart_tests;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_tests.calculate()}!');
+}
---
+++ test/dart_tests_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_tests/dart_tests.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
